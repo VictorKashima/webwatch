a0406544ef80a1ea982c56f5b2755c1f5cea5397
Victor Kashima <victorkashima@pop-os.localdomain>
2023-02-08 21:20:02 -0300

    WatchRunning

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,15 @@
 const hour = document.getElementById("hour");
 const minute = document.getElementById("minute");
 const second = document.getElementById("second");
+const utc = document.getElementById("utc");
+const year = document.getElementById("year");
+const month = document.getElementById("month");
+const day = document.getElementById("day");
+const play = document.getElementById("play");
+var dayWeek = $(".dayWeek");
 
-const clock = setInterval(function() {
+
+setInterval(function() {
 
     let dateToday = new Date();
 
@@ -15,8 +22,33 @@ const clock = setInterval(function() {
     let nowSecond = dateToday.getSeconds();
     nowSecond = ("00" + nowSecond).slice(-2);
 
+    let nowUtc = dateToday.getTimezoneOffset();
+    
+    let nowYear = dateToday.getFullYear();
+
+    let nowMonth = (dateToday.getMonth()) + 1;
+    nowMonth = ("00" + nowMonth).slice(-2);
+
+    let nowDay = dateToday.getDate();
+    nowDay = ("00" + nowDay).slice(-2);
+
+    var nowWeekday = dateToday.getDay();
+    $(dayWeek).removeClass("nowDayWeek");
+    $(dayWeek[nowWeekday]).addClass("nowDayWeek");
+
+    year.textContent = nowYear;
+    month.textContent = nowMonth;
+    day.textContent = nowDay;
     hour.textContent = nowHour;
     minute.textContent = nowMinute;
     second.textContent = nowSecond;
 
 })
+
+function playMusic() {
+    let audio = new Audio("/static/sound/clock.mp3");
+    audio.loop = true;
+    audio.play();   
+}
+
+play.addEventListener("click", playMusic);
